fe07710fba34e8c046b293fa62f0fd428c1b71bb
mxlskh <mihail.lida21@mail.ru>
2025-05-02 21:33:59 +0300

    123

--- bot/main.py
+++ bot/main.py
@@ -13,6 +13,11 @@ def main():
     load_dotenv()
     print("TELEGRAM_BOT_TOKEN =", os.getenv("TELEGRAM_BOT_TOKEN"))
     print("OPENAI_API_KEY =", os.getenv("OPENAI_API_KEY"))
+    print("DEBUG: вся env:")
+    for key in os.environ:
+        if "TELEGRAM" in key:
+            print(key, "=", os.environ[key])
+
 
     # Setup logging
     logging.basicConfig(
