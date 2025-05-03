ae4bf991034b35c6e7d443849f3e65e0c8eab4ec
mxlskh <mihail.lida21@mail.ru>
2025-05-03 21:45:07 +0300

    язык

--- bot/main.py
+++ bot/main.py
@@ -50,7 +50,7 @@ def main():
         'functions_max_consecutive_calls': int(os.environ.get('FUNCTIONS_MAX_CONSECUTIVE_CALLS', 10)),
         'presence_penalty': float(os.environ.get('PRESENCE_PENALTY', 0.0)),
         'frequency_penalty': float(os.environ.get('FREQUENCY_PENALTY', 0.0)),
-        'bot_language': os.environ.get('BOT_LANGUAGE', 'en'),
+        'bot_language': os.environ.get('BOT_LANGUAGE', 'ru'),
         'show_plugins_used': os.environ.get('SHOW_PLUGINS_USED', 'false').lower() == 'true',
         'whisper_prompt': os.environ.get('WHISPER_PROMPT', ''),
         'vision_model': os.environ.get('VISION_MODEL', 'gpt-4o'),
@@ -99,7 +99,7 @@ def main():
         'tts_model': os.environ.get('TTS_MODEL', 'tts-1'),
         'tts_prices': [float(i) for i in os.environ.get('TTS_PRICES', "0.015,0.030").split(",")],
         'transcription_price': float(os.environ.get('TRANSCRIPTION_PRICE', 0.006)),
-        'bot_language': os.environ.get('BOT_LANGUAGE', 'en'),
+        'bot_language': os.environ.get('BOT_LANGUAGE', 'ru'),
     }
 
     plugin_config = {
