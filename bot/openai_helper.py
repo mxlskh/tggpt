df0946d63f969f4acec621f82f9bd1bd67d3bdac
mxlskh <mihail.lida21@mail.ru>
2025-05-10 22:47:07 +0300

    Revert "dildodini"
    This reverts commit 2e909cecd67995a5b7fdb7fece73f829dccbe655.

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -334,9 +334,9 @@ class OpenAIHelper:
                 prompt=prompt,
                 n=1,
                 model=self.config['image_model'],
-                quality=self.config['image_quality'],
-                style=self.config['image_style'],
-                size=self.config['image_size']
+                #quality=self.config['image_quality'],
+                #style=self.config['image_style'],
+                #size=self.config['image_size']
             )
 
             if len(response.data) == 0:
