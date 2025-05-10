99d1be70f1694d38e12bd958bcff2b9e2c2dfa45
mxlskh <mihail.lida21@mail.ru>
2025-05-10 22:36:17 +0300

    style

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -335,8 +335,8 @@ class OpenAIHelper:
                 n=1,
                 model=self.config['image_model'],
                 #quality=self.config['image_quality'],
-                style=self.config['image_style'],
-                size=self.config['image_size']
+                #style=self.config['image_style'],
+                #size=self.config['image_size']
             )
 
             if len(response.data) == 0:
